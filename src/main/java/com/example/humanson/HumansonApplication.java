2b056c4216faf258fa1da2e61be2a40a8b5a5cc2
yongrakk <c1.yrkk@gmail.com>
2024-09-21 20:34:17 +0900

    커밋

---
+++ src/main/java/com/example/humanson/HumansonApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumansonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumansonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/humanson/HumansonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumansonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
